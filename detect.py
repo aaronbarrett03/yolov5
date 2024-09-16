7cd53c6c105a2f8440aeeebfd51c31b20dee6f5e
aaronbarrett03 <100042624+aaronbarrett03@users.noreply.github.com>
2024-09-16 16:21:41 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -69,14 +69,14 @@ from utils.torch_utils import select_device, smart_inference_mode
 @smart_inference_mode()
 def run(
     weights=ROOT / "yolov5s.pt",  # model path or triton URL
-    source=1,
+    source=0,
     data=ROOT / "data/coco128.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
     conf_thres=0.25,  # confidence threshold
     iou_thres=0.45,  # NMS IOU threshold
     max_det=1000,  # maximum detections per image
     device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-    view_img=False,  # show results
+    view_img=True,  # show results
     save_txt=False,  # save results to *.txt
     save_csv=True,  # save results in CSV format
     save_conf=True,  # save confidences in --save-txt labels
